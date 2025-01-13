4d8e88e7002ec192511ac596e12fb1b18fe5af31
Snidero <e.snidero@eye-tech.it>
2025-01-13 11:48:32 +0100

    Automatic copier, Mattia's information added

--- copy.sh
+++ copy.sh
@@ -8,13 +8,16 @@ GREEN="\033[92m"
 # Prompt the user for the GitHub repository name and the library repository name
 read -p "$(echo $BOLD$CYAN"Enter the new GitHub url: "$RESET)" github_repo_url
 read -p "$(echo $BOLD$CYAN"Enter the library name (now is lightning_project_skeleton): "$RESET)" library_repo_name
+read -p "$(echo $BOLD$CYAN"Enter your name (lowercase): "$RESET)" name
+
+answers_file=copier-answers-"$name".yaml
 
 # Get the current directory
 current_dir=$(pwd)
 
 # Move the main folder to a new folder with the library repository name
 cd ..
-copier copy -fd "project_name=$library_repo_name" -fd "github_repo_name=$library_repo_name" -a .copier-answers-mattia.yaml --trust gh:Eye-Tech-AI/pyet-boilerplate-package "$library_repo_name"
+copier copy -fd "project_name=$library_repo_name" -fd "github_repo_name=$library_repo_name" -a "$answers_file" --trust gh:Eye-Tech-AI/pyet-boilerplate-package "$library_repo_name"
 cd "$library_repo_name"
 
 ## GitHub updates
