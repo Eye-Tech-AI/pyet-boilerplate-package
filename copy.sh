6e3075bbbdacf0e9c34d4a8248e0a46ae503c2cc
Snidero <e.snidero@eye-tech.it>
2025-01-13 11:47:55 +0100

    Automatic copier, Mattia's information added

---
+++ copy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RESET="\033[0m"
+BOLD="\033[1m"
+CYAN="\033[36m"
+GREEN="\033[92m"
+
+# Prompt the user for the GitHub repository name and the library repository name
+read -p "$(echo $BOLD$CYAN"Enter the new GitHub url: "$RESET)" github_repo_url
+read -p "$(echo $BOLD$CYAN"Enter the library name (now is lightning_project_skeleton): "$RESET)" library_repo_name
+
+# Get the current directory
+current_dir=$(pwd)
+
+# Move the main folder to a new folder with the library repository name
+cd ..
+copier copy -fd "project_name=$library_repo_name" -fd "github_repo_name=$library_repo_name" -a .copier-answers-mattia.yaml --trust gh:Eye-Tech-AI/pyet-boilerplate-package "$library_repo_name"
+cd "$library_repo_name"
+
+## GitHub updates
+git init
+git add .
+git commit -m "build: initial commit"
+git remote add origin "$github_repo_url"
+git branch -M master
+git push -u origin master
+
+# All done!
+echo $BOLD$GREEN"Project has been created successfully!"$RESET
