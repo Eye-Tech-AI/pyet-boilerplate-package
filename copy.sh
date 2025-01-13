56b9850e830a4443002c52ce8f0b01720adeb865
Snidero <e.snidero@eye-tech.it>
2025-01-13 12:05:43 +0100

    changed copy.sh

--- copy.sh
+++ copy.sh
@@ -6,18 +6,18 @@ CYAN="\033[36m"
 GREEN="\033[92m"
 
 # Prompt the user for the GitHub repository name and the library repository name
-read -p "$(echo $BOLD$CYAN"Enter the new GitHub url: "$RESET)" github_repo_url
-read -p "$(echo $BOLD$CYAN"Enter the library name (now is lightning_project_skeleton): "$RESET)" library_repo_name
+read -p "$(echo $BOLD$CYAN"Enter the library name (now is pyet-boilerplate-package): "$RESET)" library_repo_name
+github_repo_url="git@github.com:Eye-Tech-AI/$library_repo_name.git"
 read -p "$(echo $BOLD$CYAN"Enter your name (lowercase): "$RESET)" name
 
-answers_file=copier-answers-"$name".yaml
-
+answers_file="copier-answers-$name.yaml"
+echo $answers_file
 # Get the current directory
 current_dir=$(pwd)
 
 # Move the main folder to a new folder with the library repository name
 cd ..
-copier copy -fd "project_name=$library_repo_name" -fd "github_repo_name=$library_repo_name" -a "$answers_file" --trust gh:Eye-Tech-AI/pyet-boilerplate-package "$library_repo_name"
+copier copy -d "project_name=$library_repo_name" -d "github_repo_name=$library_repo_name" --data-file "pyet-boilerplate-package/$answers_file" --trust gh:Eye-Tech-AI/pyet-boilerplate-package "$library_repo_name"
 cd "$library_repo_name"
 
 ## GitHub updates
